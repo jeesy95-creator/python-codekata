ad8d33bc508b5660406bdd3654e5c4ba38fa9f9a
SyoonJ <jeesy95@gmail.com>
2026-05-26 10:03:02 +0900

    [프로그래머스] 정수 삼각형 풀이 제출
    - 문제 ID: 43105
    - 난이도: unknown
    - 제출일: 2026. 05. 26. 10:03:01
    - 작성자: 지소윤

--- programmers/other/43105_정수_삼각형.py
+++ programmers/other/43105_정수_삼각형.py
@@ -2,26 +2,13 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/43105
 # 작성자: 지소윤
-# 작성일: 2026. 05. 26. 10:01:32
+# 작성일: 2026. 05. 26. 10:03:01
 
 def solution(triangle):
-    dp = []
+    dp = triangle[-1][:]
 
-    for row in triangle:
-        dp.append([0] * len(row))
-
-    dp[0][0] = triangle[0][0]
-
-    for i in range(1, len(triangle)):
+    for i in range(len(triangle) - 2, -1, -1):
         for j in range(len(triangle[i])):
+            dp[j] = triangle[i][j] + max(dp[j], dp[j + 1])
 
-            if j == 0:
-                dp[i][j] = dp[i - 1][j] + triangle[i][j]
-
-            elif j == i:
-                dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
-
-            else:
-                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
-
-    return max(dp[-1])
+    return dp[0]
